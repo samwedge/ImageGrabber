975364977d578f9df28739d85d1fd8bb0d5bb81a
samwedge <samwedge@gmail.com>
2016-11-19 17:24:56 +0000

    add test for create_directory

--- TestImageGrabber.py
+++ TestImageGrabber.py
@@ -1,8 +1,17 @@
 import unittest
+import os
+from ImageGrabber import *
 
 
 class MyTestCase(unittest.TestCase):
-    pass
+
+    def test_create_directory(self):
+        dirname = 'aL6D45kaH2F1jUU1eMDf'
+        self.assertFalse(os.path.exists(dirname))
+        create_directory(dirname)
+        self.assertTrue(os.path.exists(dirname))
+        os.removedirs(dirname)
+        self.assertFalse(os.path.exists(dirname))
 
 
 if __name__ == '__main__':
